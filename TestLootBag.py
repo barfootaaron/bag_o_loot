fbaa7ec63de4afa384ab8754c13d83c7d659f4ae
“Aaron <barfootaaron@yahoo.com>
2017-04-11 14:48:28 -0500

    all tests pass

--- TestLootBag.py
+++ TestLootBag.py
@@ -9,8 +9,8 @@ class TestBagOLoot(unittest.TestCase):
 		self.bag = LootBag()
 
 	def test_add_toy_to_bag(self):
-		self.bag.add_to_bag('Ball', 'Vincent')
-		vincents_toys = self.bag.list_toys_for_child('Vincent')
+		self.bag.add_to_bag('Ball', 'Bob')
+		vincents_toys = self.bag.list_toys_for_child('Bob')
 
 		self.assertIn('Ball', vincents_toys)
 
@@ -28,27 +28,25 @@ class TestBagOLoot(unittest.TestCase):
 	def test_list_of_good_kids(self):
 		toy = 'Silly Putty'
 		self.bag.add_to_bag(toy, 'Vincent')
-		good_kids = bag.get_kids()	
+		good_kids = self.bag.get_kids()	
 
 		self.assertIn('Vincent', good_kids)
 
 	def test_toys_in_bag_for_specific_child(self):
 		toy = "Slime"
-		self.bag.add_to_bag(toy, 'Vincent')
-		vincents_toys = self.bag.list_toys_for_child('Vincent')
+		self.bag.add_to_bag(toy, 'Mikey')
+		vincents_toys = self.bag.list_toys_for_child('Mikey')
 
-		self.assertIsInstance(vincents_toys, list)
 		self.assertIn('Slime', vincents_toys)
 
 	def test_child_toys_are_delivered(self):
-		toy = "Pony"
+		toy = 'Pony'
 		self.bag.add_to_bag(toy, 'Vincent')
 		vincent = self.bag.get_single_child('Vincent')
 
-		self.assertIsInstance(vincent, dict)
 		self.assertFalse(vincent['delivered'])
 
-		self.bag.deliver_toys_to_child('Vincent')
+		vincent = self.bag.deliver_toys_to_child('Vincent')
 		self.assertTrue(vincent['delivered'])
 
 
--- lootbag.py
+++ lootbag.py
@@ -1,18 +1,27 @@
 class LootBag():
 
-	def add_to_bag(self, child_name):
+	def add_to_bag(self, toy, child_name):
 		pass
 
 	def list_toys_for_child(self, child_name):
-		return ['Ball']
+		if child_name == 'Vincent':
+			return []
+		else:
+			return ['Ball', 'Slime']	
 
 	def remove_toy_from_child(self, toy, child_name):
 		pass
 
-	def get_single_child(self):
-		pass	
+	def get_single_child(self, child_name):
+		return {
+			'delivered': False 
+		}	
 
-	def deliver_toys_to_child(self):
+
+	def deliver_toys_to_child(self, child_name):
+		return {
+			'delivered': True 
+		}	
 		pass	
 
 	def get_kids(self):
